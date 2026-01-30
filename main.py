31251e970891399d76772a58df5fc0be695d6e76
JawBlade <122082998+JawBlade@users.noreply.github.com>
2026-01-30 09:35:26 -0800

    Trying to figure out the game logic

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from rich.align   import Align
 from rich.bar     import Bar
 from character    import characters
 from rich.columns import Columns
-from pynput       import keyboard
-from keylogger    import KeyLogger
 from rich         import print
+from rich.prompt  import Prompt
 from rich.panel   import Panel
+import random
 from rich.layout  import Layout
 
 #change the end value to creat the health bar move.
@@ -47,7 +47,8 @@ layout.split_column(
         name="title",
         ratio=1
     ),
-    Layout(name="bottom", ratio=2)
+    Layout(name="bottom", ratio=2),
+    Layout(name="choice", ratio=1)
 )
 
 def main():
@@ -56,18 +57,28 @@ def main():
 
     player_layout = Layout(Panel(Align.center(Player, vertical='top'), title= player.name, style= player.color))
     enemy_layout = Layout(Panel(Align.center(Enemy, vertical='top'), title= enemy.name, style= enemy.color))
+    choice_layout = Layout(Panel(Align.center("Choose an action (press Enter)", vertical='middle'), title= "Options", style= 'green'))
     
     layout["bottom"].split_row(
         player_layout,
         enemy_layout
     )
 
+    layout['choice'].split_row(choice_layout)
+
     print(layout)
 
-    logger = KeyLogger("Player")
+    turn = 0 
+    while True:
+        if turn % 2 == 0:
+            layout['choice'].update(Panel(Align.center("Your turn! Choose an action:", vertical='middle'), title= "Options", style= 'green'))
+        else:
+            layout['choice'].update(Panel(Align.center("Enemy's turn! Press Enter to continue.", vertical='middle'), title= "Options", style= 'red'))
+            
 
-    with keyboard.Listener(on_press=logger.on_press) as listener:
-       listener.join()
+        choice = input('')
+        print(layout)
+
 
 if __name__ == '__main__':
     main()
